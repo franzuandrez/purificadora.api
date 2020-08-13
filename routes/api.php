fd391ef5cec88e005a8e3335b147d29c4203af5f
Edson Andrez <edson.andrez@outlook.es>
2020-08-13 00:21:41 -0600

    remove user default route

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 
 Route::post('customer', 'CustomerController@store')->name('customer.store');
