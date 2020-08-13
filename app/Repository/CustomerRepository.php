751a78e68de338996671d72478be28a03f26390e
Edson Andrez <edson.andrez@outlook.es>
2020-08-12 23:40:32 -0600

    Repository for Customer

---
+++ app/Repository/CustomerRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Repository;
+
+
+use App\Customer;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class CustomerRepository
+{
+
+
+    public function storeFromRequest(Request $request)
+    {
+        $customer = new Customer();
+        $customer = $this->save($request, $customer);
+
+        return $customer;
+    }
+
+    private function save(Request $request, Customer $customer)
+    {
+        $customer->name = $request->name;
+        $customer->last_name = $request->last_name;
+        $customer->nickname = $request->nickname;
+        $customer->address = $request->address;
+        $customer->latitude = $request->latitude;
+        $customer->longitude = $request->longitude;
+        $customer->last_date_visited = Carbon::now();
+        $customer->save();
+        return $customer;
+    }
+
+}
