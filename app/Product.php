bfe6822986a7ce1579cd784ad113d16056755830
Edson Andrez <edson.andrez@outlook.es>
2020-08-28 11:45:32 -0600

    Product Model.

---
+++ app/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //
+    protected $table = 'product';
+    protected $primaryKey = 'product_id';
+
+    protected $fillable = [
+        'description',
+        'price'
+    ];
+
+
+}
